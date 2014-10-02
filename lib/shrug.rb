d174de7d07f622a7d87fa7e057b2668fb617d2b4
Nick Elser <nick.elser@gmail.com>
2014-10-01 18:37:29 -0700

    smarter shrug!

--- lib/shrug.rb
+++ lib/shrug.rb
@@ -2,9 +2,12 @@ require "robustly"
 require "shrug/version"
 
 module Shrug
+  WHITELIST = %w(to_ary constantize to_str)
+
   def method_missing(method, *args, &block)
-    STDERR.puts('¯\_(ツ)_/¯')
-    STDERR.puts method
+    return if WHITELIST.include?(method.to_s)
+
+    STDERR.puts("¯\\_(ツ)_/¯ (`#{method.to_s}' undefined)")
     Robustly.report_exception(NoMethodError.new("undefined method `#{method}' for nil:NilClass (¯\\_(ツ)_/¯)"))
 
     self # magic happens here
